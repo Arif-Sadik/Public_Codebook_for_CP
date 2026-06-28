52a47409cf0a77045dbb4b689f712b457b5a3546
Md. Arif Sadik Molla <arifsadik244@gmail.com>
2026-06-28 23:05:19 +0600

    template
    Removed the date comment from the file.

---
+++ template_Sadik.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define dubb long double
+#define pi acos(-1)
+using namespace std;
+
+// Md. Arif Sadik Molla
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n,k,x, c, s, d, t;
+    cin>>t;
+    while(t--)
+    {
+        c=0, s=0, d=0;
+        
+        
+    }
+
+    return 0;
+}
